0d21e3caa768b12ca7be229da7fb42960055ebbe
sonali <sonali.shinde0910@gmail.com>
2026-08-24 14:36:44 +0530

    added flask app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")               #Basic application response
+def home():
+    return "Hello from Flask application!"
+
+
+@app.route("/health")         #Health-check endpoint for Kubernetes probes
+def health():
+    return jsonify({
+        "status": "UP"
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080) 
+    #Allows the app to be accessed outside the container & 8080 is application port
